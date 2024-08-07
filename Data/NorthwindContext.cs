63b95ced9dd9eccb45e9c8f9b3c9ca765988c7ac
iaroslav1970 <barayaros@gmail.com>
2024-08-07 16:29:40 +0300

    Adding file NorthwindContext.cs

---
+++ Data/NorthwindContext.cs
@@ -0,0 +1,60 @@
+using Microsoft.EntityFrameworkCore;
+using NorthwindWPF.Models;
+
+public class NorthwindContext : DbContext
+{
+    public NorthwindContext (DbContextOptions<NorthwindContext> options)
+            : base(options)
+        {
+        }
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Customer> Customers { get; set; }
+    public DbSet<Employee> Employees { get; set; }
+    public DbSet<Shipper> Shippers { get; set; }
+    public DbSet<Supplier> Suppliers { get; set; }
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<OrderDetail> OrderDetails { get; set; }
+
+    
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Product>()
+            .HasOne(p => p.Category)
+            .WithMany(c => c.Products)
+            .HasForeignKey(p => p.CategoryID);
+
+        modelBuilder.Entity<Product>()
+            .HasOne(p => p.Supplier)
+            .WithMany(s => s.Products)
+            .HasForeignKey(p => p.SupplierID);
+
+        modelBuilder.Entity<Order>()
+            .HasOne(o => o.Customer)
+            .WithMany(c => c.Orders)
+            .HasForeignKey(o => o.CustomerID);
+
+        modelBuilder.Entity<Order>()
+            .HasOne(o => o.Employee)
+            .WithMany(e => e.Orders)
+            .HasForeignKey(o => o.EmployeeID);
+
+        modelBuilder.Entity<Order>()
+            .HasOne(o => o.Shipper)
+            .WithMany(s => s.Orders)
+            .HasForeignKey(o => o.ShipperID);
+
+        modelBuilder.Entity<OrderDetail>()
+            .HasOne(od => od.Order)
+            .WithMany(o => o.OrderDetails)
+            .HasForeignKey(od => od.OrderID);
+
+        modelBuilder.Entity<OrderDetail>()
+            .HasOne(od => od.Product)
+            .WithMany(p => p.OrderDetails)
+            .HasForeignKey(od => od.ProductID);
+
+        
+    }
+}
--- obj/Debug/net8.0-windows/NorthwindWPF.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/NorthwindWPF.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NorthwindWPF")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45091b693d6765c76e5222d676df37431e53f874")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0cff3f6113d2ba6f5d33453a4ff26dc315747561")]
 [assembly: System.Reflection.AssemblyProductAttribute("NorthwindWPF")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NorthwindWPF")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
